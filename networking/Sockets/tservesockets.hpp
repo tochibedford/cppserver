10835904f449686925c0e7271715978a9a5663a2
tochibedford <musicofbyte@gmail.com>
2026-08-22 06:54:51 -0700

    Add header file for socket classes: tservesockets.hpp

---
+++ networking/Sockets/tservesockets.hpp
@@ -0,0 +1,10 @@
+#ifndef tservesockets_hpp
+#define tservesockets_hpp
+
+#include "BindingSocket.hpp"
+#include "ConnectingSocket.hpp"
+#include "ListeningSocket.hpp"
+#include "simplesocket.hpp"
+
+
+#endif
